47ad504214f466dd7d6af8bc51dcde8ee122f49a
Marius <marius@Assists-Mac-mini-2471.local>
2018-08-29 10:32:02 +0300

    inital commit

---
+++ Messenger/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Messenger
+//
+//  Created by Marius on 29/08/2018.
+//  Copyright © 2018 Marius. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
